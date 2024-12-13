aa4856755e9790b77d5b80b2189a0d733a0e2720
Shai <shay.kleiman@gmail.com>
2024-12-14 00:54:27 +0200

    Refactored Question Editor

--- lib/content/question_editor_content.dart
+++ lib/content/question_editor_content.dart
@@ -9,34 +9,36 @@ class QuestionEditorContent extends StatefulWidget {
 }
 
 class _QuestionEditorContentState extends State<QuestionEditorContent> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _questionController = TextEditingController();
   int _selectedWrongAnswerCount = 1;
   final List<int> _wrongAnswerCounts = [1, 2, 3, 4];
-  final List<TextEditingController> _answerControllers =
-      List.generate(5, (_) => TextEditingController());
+
+  final TextEditingController _questionController = TextEditingController();
+  final TextEditingController _rightAnswerController = TextEditingController();
+  final List<TextEditingController> _wrongAnswerControllers =
+      List.generate(4, (_) => TextEditingController());
+
   bool _allFieldsFilled = false;
 
   @override
   void initState() {
     super.initState();
     _questionController.addListener(_updateButtonStates);
-    for (var controller in _answerControllers) {
+    _rightAnswerController.addListener(_updateButtonStates);
+    for (var controller in _wrongAnswerControllers) {
       controller.addListener(_updateButtonStates);
     }
   }
 
   void _updateButtonStates() {
     bool allFilled = _questionController.text.isNotEmpty &&
-        _answerControllers[0].text.isNotEmpty;
+        _rightAnswerController.text.isNotEmpty;
 
-    for (int i = 1; i <= _selectedWrongAnswerCount; i++) {
-      if (_answerControllers[i].text.isEmpty) {
+    for (int i = 0; i < _selectedWrongAnswerCount; i++) {
+      if (_wrongAnswerControllers[i].text.isEmpty) {
         allFilled = false;
         break;
       }
     }
-
     if (mounted) {
       setState(() {
         _allFieldsFilled = allFilled;
@@ -52,142 +54,103 @@ class _QuestionEditorContentState extends State<QuestionEditorContent> {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: _questionController,
-                decoration: InputDecoration(
-                  labelText: 'Question',
-                  border: const OutlineInputBorder(),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue.shade200),
-                  ),
-                  focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue),
-                  ),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a question title';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                controller: _answerControllers[0],
-                decoration: InputDecoration(
-                  labelText: 'Answer',
-                  border: const OutlineInputBorder(),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green.shade200),
-                  ),
-                  focusedBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green),
-                  ),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an answer';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 10),
-              const SizedBox(width: 10),
-              SegmentedButton<int>(
-                segments: _wrongAnswerCounts.map((count) {
-                  return ButtonSegment<int>(
-                    value: count,
-                    label: Text(count.toString()),
-                  );
-                }).toList(),
-                selected: {_selectedWrongAnswerCount},
-                onSelectionChanged: (newSelection) {
-                  setState(() {
-                    _selectedWrongAnswerCount = newSelection.first;
-                  });
-                  _updateButtonStates();
-                },
-                showSelectedIcon: true,
-                style: ButtonStyle(
-                  shape: WidgetStateProperty.all(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50)),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Column(
-                children: List.generate(_selectedWrongAnswerCount, (index) {
-                  return TextFormField(
-                    controller: _answerControllers[index + 1],
-                    decoration: InputDecoration(
-                      labelText: 'Wrong Answer',
-                      border: const OutlineInputBorder(),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red.shade200),
-                      ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red),
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter an answer';
-                      }
-                      return null;
-                    },
-                  );
-                }),
-              ),
-              const SizedBox(height: 20),
-            ],
-          ),
+        child: Column(
+          children: _buildFormFields(),
         ),
       ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: ElevatedButton(
-                onPressed: !_allFieldsFilled
-                    ? () {
-                        // Handle generate action when fields are incomplete
-                      }
-                    : null,
-                child: const Text('Generate'),
-              ),
-            ),
-            const SizedBox(width: 10),
-            Expanded(
-              child: ElevatedButton(
-                onPressed: _allFieldsFilled
-                    ? () {
-                        if (_formKey.currentState!.validate()) {
-                          final newQuestion = Question(
-                            _questionController.text,
-                            _answerControllers[0].text,
-                            _answerControllers
-                                .sublist(1, _selectedWrongAnswerCount + 1)
-                                .map((c) => c.text)
-                                .toList(),
-                          );
-                          
-                          Navigator.pop(context, newQuestion);
-                        }
-                      }
-                    : null,
-                child: const Text('Save'),
-              ),
-            ),
-          ],
+      bottomNavigationBar: _buildActionButtons(),
+    );
+  }
+
+  List<Widget> _buildFormFields() {
+    return [
+      TextField(
+        controller: _questionController,
+        decoration: const InputDecoration(
+          labelText: 'Question',
+          border: OutlineInputBorder(),
         ),
+      ),
+      const SizedBox(height: 10),
+      TextField(
+        controller: _rightAnswerController,
+        decoration: const InputDecoration(
+          labelText: 'Answer',
+          border: OutlineInputBorder(),
+        ),
+      ),
+      const SizedBox(height: 30),
+      SegmentedButton<int>(
+        segments: _wrongAnswerCounts.map((count) {
+          return ButtonSegment<int>(
+            value: count,
+            label: Text(count.toString()),
+          );
+        }).toList(),
+        selected: {_selectedWrongAnswerCount},
+        onSelectionChanged: (newSelection) {
+          setState(() {
+            _selectedWrongAnswerCount = newSelection.first;
+          });
+          _updateButtonStates();
+        },
+        showSelectedIcon: true,
+      ),
+      const SizedBox(height: 30),
+      Column(
+        children: List.generate(_selectedWrongAnswerCount, (index) {
+          return Padding(
+              padding: const EdgeInsets.only(bottom: 15),
+              child: TextField(
+                controller: _wrongAnswerControllers[index],
+                decoration: const InputDecoration(
+                  labelText: 'Wrong Answer',
+                  border: OutlineInputBorder(),
+                ),
+              ));
+        }),
+      ),
+      const SizedBox(height: 20),
+    ];
+  }
+
+  Widget _buildActionButtons() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 55),
+      child: Row(
+        children: [
+          Expanded(
+            child: ElevatedButton(
+              onPressed: !_allFieldsFilled ? _handleGeneratePress : null,
+              child: const Text('Generate'),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: ElevatedButton(
+              onPressed: _allFieldsFilled ? _handleSavePress : null,
+              child: const Text('Save'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  void _handleGeneratePress() {
+    // Handle generate action when fields are incomplete
+  }
+
+  void _handleSavePress() {
+    final newQuestion = Question(
+      _questionController.text,
+      _rightAnswerController.text,
+      _wrongAnswerControllers
+          .sublist(0, _selectedWrongAnswerCount)
+          .map((c) => c.text)
+          .toList(),
+    );
+
+    Navigator.pop(context, newQuestion);
   }
 }
