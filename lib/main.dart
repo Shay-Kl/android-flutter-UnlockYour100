7a5f43cbc0ddc423012d1e056d6cad08a3c04454
Shai <shay.kleiman@gmail.com>
2024-12-17 15:56:13 +0200

    Changed theme to blue

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginScreen(),
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: const LoginScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
