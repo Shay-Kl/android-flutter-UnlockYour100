27169b5578f797d29e42fcd12375bdcffd3a600a
Shai <shay.kleiman@gmail.com>
2024-12-27 13:31:05 +0200

    Cleaned up SetListScreen and made it use FutureBuilder to load sets

--- lib/screens/set_list_screen.dart
+++ lib/screens/set_list_screen.dart
@@ -1,73 +1,28 @@
-// import 'package:flutter/material.dart';
-// import 'question_list_screen.dart';
-
-// class QuestionSetListContent extends StatelessWidget {
-//   const QuestionSetListContent({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Question Set List Screen'),
-//         actions: [
-//           IconButton(
-//             icon: const Icon(Icons.settings),
-//             onPressed: () {
-//               // Handle settings action
-//             },
-//           ),
-//         ],
-//       ),
-//       body: Center(
-//         child: ElevatedButton(
-//           child: const Text('Edit Set'),
-//           onPressed: () {
-//             Navigator.push(
-//               context,
-//               MaterialPageRoute(builder: (context) => const QuestionListContent()),
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:project/screens/question_list_screen.dart';
 import '../models/set.dart';
-import '../models/question.dart';
 import 'package:provider/provider.dart';
 import '../providers/auth_provider.dart';
-import '../providers/question_provider.dart';
 import '../providers/set_provider.dart';
 
-class QuestionSetListContent extends StatefulWidget {
-  const QuestionSetListContent({super.key});
+class SetListScreen extends StatefulWidget {
+  const SetListScreen({super.key});
 
   @override
-  State<QuestionSetListContent> createState() => _QuestionSetListContentState();
+  State<SetListScreen> createState() => _SetListScreenState();
 }
 
-class _QuestionSetListContentState extends State<QuestionSetListContent> {
+class _SetListScreenState extends State<SetListScreen> {
   //final authStatus = context.watch<AuthentificationNotifier>();
   //List<QuestionSet> questionSets = authStatus.Sets;
 
-  List<QuestionSet> questionSets = [];
+  late Future<List<QuestionSet>> questionSets;
 
   @override
   void initState() {
     super.initState();
-    _fetchSetsFromFirestore();
-  }
-
-  Future<void> _fetchSetsFromFirestore() async {
-    final userEmail = Provider.of<AuthProvider>(context, listen: false).userEmail;
     final provider = Provider.of<SetProvider>(context, listen: false);
-    final fetchedSets = await provider.readSetsForUser(userEmail!);
-    setState(() {
-      questionSets = fetchedSets;
-    });
+    questionSets = provider.readSetsForUser('shay.kleiman@gmail.com');
   }
 
   void _showNewSetDialog() {
@@ -97,9 +52,14 @@ class _QuestionSetListContentState extends State<QuestionSetListContent> {
                     // Add a new QuestionSet with the entered name
                     // questionSets.add(QuestionSet(name: newName, questions: []));
                     final newSet = QuestionSet(setName: newName, questions: []);
-                    questionSets.add(newSet);
-                    var userEmail = Provider.of<AuthProvider>(context, listen: false).userEmail;
-                    Provider.of<SetProvider>(context, listen: false).createSet(userEmail!, newSet);
+                    questionSets.then((sets) {
+                      sets.add(newSet);
+                    });
+                    var userEmail =
+                        Provider.of<AuthProvider>(context, listen: false)
+                            .userEmail;
+                    Provider.of<SetProvider>(context, listen: false)
+                        .createSet(userEmail!, newSet);
                   });
                 }
                 Navigator.pop(context); // Close the dialog
@@ -126,10 +86,21 @@ class _QuestionSetListContentState extends State<QuestionSetListContent> {
           ),
         ],
       ),
-      body: ListView(
-        children: questionSets.map((s) {
-          return SetCard(s);
-        }).toList(),
+      body: FutureBuilder<List<QuestionSet>>(
+        future: questionSets,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView(
+              children: snapshot.data!.map((s) {
+                return SetCard(s);
+              }).toList(),
+            );
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _showNewSetDialog,
