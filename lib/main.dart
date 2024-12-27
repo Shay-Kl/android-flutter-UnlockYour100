37ebda4b04f8bb4eea50ea72577e3fa06f99704b
Shai <shay.kleiman@gmail.com>
2024-12-27 12:26:18 +0200

    Refactored main

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,11 @@ import 'providers/auth_provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MainApp());
+  runApp(MyAppInit());
 }
 
-class MainApp extends StatelessWidget {
-  MainApp({super.key});
+class MyAppInit extends StatelessWidget {
+  MyAppInit({super.key});
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
@@ -20,31 +20,16 @@ class MainApp extends StatelessWidget {
       future: _initialization,
       builder: (context, snapshot) {
         if (snapshot.hasError) {
-          return Scaffold(body: Center(child: Text(snapshot.error.toString(), textDirection: TextDirection.ltr)));
+          return Scaffold(
+              body: Center(
+                  child: Text(snapshot.error.toString(),
+                      textDirection: TextDirection.ltr)));
         }
         if (snapshot.connectionState == ConnectionState.done) {
-          return const MyNotifierApp();
+          return const MyApp();
         }
         return const Center(child: CircularProgressIndicator());
       },
-    );
-  }
-}
-
-class MyNotifierApp extends StatelessWidget {
-  const MyNotifierApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-        ChangeNotifierProvider(create: (context) => QuestionProvider()),
-        // add set provider
-        ChangeNotifierProvider(create: (context) => SetProvider()),
-      ],
-      child: const MaterialApp(
-        home: MyApp(),
-      ),
     );
   }
 }
@@ -53,12 +38,19 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (context) => QuestionProvider()),
+        ChangeNotifierProvider(create: (context) => SetProvider()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        ),
+        home: const LoginScreen(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const LoginScreen(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
